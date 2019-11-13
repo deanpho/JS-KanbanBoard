8e12580f960a49b30b1ea7151bc2cc3a259a2602
Dean Pho <nguyendeanpho@gmail.com>
2019-11-13 16:28:29 -0500

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -37,7 +37,7 @@ const create_item = () => {
       item.innerHTML = input.value;
       adding = false;
     } else {
-      error.innerHTML('message');
+      error.innerHTML= 'message';
     }
 
   })
