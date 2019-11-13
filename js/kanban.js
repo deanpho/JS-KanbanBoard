66125888bcb528477a7321e55782f6e23eb122c2
Dean Pho <nguyendeanpho@gmail.com>
2019-11-13 17:05:53 -0500

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -49,8 +49,8 @@ document.querySelectorAll('.drop').forEach(element => {
   element.addEventListener('drop', event => {
     event.preventDefault();
     const id = event.dataTransfer.getData('text');
-    event.target.appendChild(document.getElementbyId(id));
+    event.target.appendChild(document.getElementById(id));
   });
-  element.addEventListener('dragover', event => {
-    event.preventdefault();
+  element.addEventListener('dragover', event =>
+  event.preventdefault());
 });
